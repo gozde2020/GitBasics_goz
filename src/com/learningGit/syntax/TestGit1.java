fc249e12b9bf08c08a6692902eff8dba6e6b37de
gozde2020 <ercevik.gozde@gmail.com>
2020-06-11 14:19:46 -0700

    commiting change to Test1

--- src/com/learningGit/syntax/TestGit1.java
+++ src/com/learningGit/syntax/TestGit1.java
@@ -3,8 +3,8 @@ package com.learningGit.syntax;
 public class TestGit1 {
 
 	public static void main(String[] args) {
-		System.out.println("Letslearn git");
-		System.out.println("Making aa change");
+		System.out.println("Making a change for remote repo");
+		
 
 	}
 
