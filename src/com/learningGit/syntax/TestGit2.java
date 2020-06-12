b05f8e12db55570ffab1eaca1609496b4731399e
gozde2020 <ercevik.gozde@gmail.com>
2020-06-12 14:58:47 -0700

    bu da cikarsa anladim bu konuyu demektir

--- src/com/learningGit/syntax/TestGit2.java
+++ src/com/learningGit/syntax/TestGit2.java
@@ -3,7 +3,7 @@ package com.learningGit.syntax;
 public class TestGit2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("bu da cikarsa ben bu konuyu anladim");
 
 	}
 
