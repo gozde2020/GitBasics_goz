06e03303a42f8734d009bffe9db648e5cb1cf545
gozde2020 <ercevik.gozde@gmail.com>
2020-06-12 14:54:51 -0700

    3. dosyada degisiklik yaptim

--- src/com/learningGit/syntax/TestGit3.java
+++ src/com/learningGit/syntax/TestGit3.java
@@ -2,9 +2,9 @@ package com.learningGit.syntax;
 
 public class TestGit3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) { 
+		
+		System.out.println("Cok merak ediyorum ne cikacak:)");
 	}
 
 }
